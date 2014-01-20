08fde0b568e1c6d5d0e1446ed527d40e9fba3af2
feay1234 <feay1234@gmail.com>
2014-01-20 12:46:54 +0000

    add new admin

--- wsgi/mysite/Lingo/admin.py
+++ wsgi/mysite/Lingo/admin.py
@@ -3,3 +3,6 @@ from Lingo.models import *
 
 admin.site.register(UserProfile)
 admin.site.register(Message)
+admin.site.register(Language)
+admin.site.register(Favorite)
+admin.site.register(FriendInvitation)
